0949bb9ed1cf51139ce278521e92b206e36133a6
LooTan_H <loosung@naver.com>
2024-08-05 14:06:24 +0900

    Session 3: Thread 4. Test

--- src/main/java/thread/start/control/CheckedExceptionMain.java
+++ src/main/java/thread/start/control/CheckedExceptionMain.java
@@ -9,7 +9,8 @@ public class CheckedExceptionMain {
 
 	/*
 	* Thread 의 강제 종료가 올 수 있고 멀티스레딩 환경에서는 예외 처리를 강조함으로써 스레드의 안정성 및 일관성 유지!
-	* 오버라딩 된 메서드가 예외를 처리 하지않으면 하위 메서드는 처리 할 수 없다. (자바의 Exception 규칙)*/
+	* 오버라딩 된 메서드가 예외를 처리 하지않으면 하위 메서드는 처리 할 수 없다. (자바의 Exception 규칙)
+	* */
 
 	static class TempCheckedRunnable implements Runnable {
 		@Override
