f52bec3a91f1f52fe737cb4145e06b82b3f66f24
emoldino-louis <woosung.choi@onepredict.com>
2024-08-05 13:57:54 +0900

    Session 3: Thread 3. Runnable run() 체크 예외 재정의 - Thread 의 강제 종료가 올 수 있고 멀티스레딩 환경에서는 예외 처리를 강조함으로써 스레드의 안정성 및 일관성 유지!
    * 오버라딩 된 메서드가 예외를 처리 하지않으면 하위 메서드는 처리 할 수 없다. (자바의 Exception 규칙)

--- src/main/java/thread/start/control/CheckedExceptionMain.java
+++ src/main/java/thread/start/control/CheckedExceptionMain.java
@@ -1,5 +1,7 @@
 package thread.start.control;
 
+import static util.ThreadUtils.sleep;
+
 public class CheckedExceptionMain {
 	public static void main(String[] args) throws Exception {
 		throw new Exception();
@@ -9,10 +11,18 @@ public class CheckedExceptionMain {
 	* Thread 의 강제 종료가 올 수 있고 멀티스레딩 환경에서는 예외 처리를 강조함으로써 스레드의 안정성 및 일관성 유지!
 	* 오버라딩 된 메서드가 예외를 처리 하지않으면 하위 메서드는 처리 할 수 없다. (자바의 Exception 규칙)*/
 
-	static class CheckedRunnable implements Runnable {
+	static class TempCheckedRunnable implements Runnable {
 		@Override
 		public void run() /*throws Exception*/ {
 			/*throw new Exception();*/
+		}
+	}
+
+	// Util 로 만들어 진행.
+	static class CheckedRunnable implements Runnable {
+		@Override
+		public void run() {
+			sleep(1000);
 		}
 	}
 }
--- src/main/java/util/ThreadUtils.java
+++ src/main/java/util/ThreadUtils.java
@@ -1,4 +1,15 @@
 package util;
 
-public class ThreadUtils {
+import static util.LoggerUtils.log;
+
+public abstract class ThreadUtils {
+
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException ex) {
+			log("인터럽트 발생, " + ex.getMessage());
+			throw new RuntimeException(ex);
+		}
+	}
 }
