4ad0e68a740d639137df23e5289849e37c1f585d
LooTan_H <loosung@naver.com>
2024-08-07 09:36:59 +0900

    Session 5 <Volatile : 메모리 가시성> 2. volatile(메모리 가시성) : CPU는 처리 성능을 위해 캐시 메모리를 사용한다. 그래서 boolean으로만 설정한 "flag"는 MyTask(Thread)에서 읽지 않고, Cache Memory 에서 가져온다.
    * 결론 "VolatileFlagMain" 메인 스레드는 절대로 멈추지 않았다... 언제 flag가 반영될지 모른다... 평생 while을 탈출을 못 할 수 도있다.
    
    현재 이런 문제를 "Memory Visibility" 라고 한다. 메모리에 변경 값이 보이냐? 안보이냐의 문제.

--- src/main/java/thread/volatiles/VolatileFlagMain.java
+++ src/main/java/thread/volatiles/VolatileFlagMain.java
@@ -21,6 +21,7 @@ public class VolatileFlagMain {
 	}
 
 	static class MyTask implements Runnable {
+		// "VolatileFlagMain" 메인 스레드는 절대로 멈추지 않았다... 언제 flag가 반영될지 모른다... 평생 while을 탈출을 못 할 수 도있다.
 		boolean flag = true;
 
 		@Override
