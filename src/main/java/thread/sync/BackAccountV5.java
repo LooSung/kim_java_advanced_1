05e3ca072c0a781a857e7c57dbfe9df819020123
LooTan_H <loosung@naver.com>
2024-08-07 16:40:04 +0900

    Session 7 <고오급 동기화 : concurrent.Lock> 2. ReentrantLock : 대기 중단.
    * if(!lock.tryLock()) - 만약 작업중인 Thread lock 이 없다면 바로 빠져나오면 false로 반환된다.

---
+++ src/main/java/thread/sync/BackAccountV5.java
@@ -0,0 +1,59 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.LoggerUtils.log;
+import static util.ThreadUtils.sleep;
+
+public class BackAccountV5 implements BackAccount {
+	private int balance;
+
+	private final Lock lock = new ReentrantLock();
+
+	public BackAccountV5(int initialBalance) {
+		this.balance = initialBalance;
+	}
+
+	@Override
+	public boolean withdraw(int amount) {
+		log("거래 시작 : " + getClass().getSimpleName());
+
+		if(!lock.tryLock()) {
+			log("[잔입 실패] : 이미 처리중인 작업이 있습니다.");
+			return false;
+		}
+
+		// ReentrantLock 이용
+		lock.lock();
+		try {
+			log("[검증 시작] 출금액 : " + amount + " 잔액 " + balance);
+			if (balance < amount) {
+				log("[검증 실패] 출금액 : " + amount + " 잔액 " + balance);
+				return false;
+			}
+			// 잔고가 출금액 보다 많으면, 진행
+			log("[검증 완료] 출금액 : " + amount + " 잔액 " + balance);
+
+			sleep(1000);
+			balance -= amount;
+			log("[출금 완료] 출금액 : " + amount + " 잔액 " + balance);
+		} finally {
+			lock.unlock();
+		}
+
+		log("거래 종료");
+		return true;
+	}
+
+	@Override
+	public int getBalance() {
+		lock.lock();
+		try {
+			return balance;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+}
--- src/main/java/thread/sync/BankMain.java
+++ src/main/java/thread/sync/BankMain.java
@@ -5,7 +5,7 @@ import static util.ThreadUtils.sleep;
 
 public class BankMain {
 	public static void main(String[] args) throws InterruptedException {
-		BackAccount account = new BackAccountV4(1000);
+		BackAccount account = new BackAccountV5(1000);
 
 		Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
 		Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
