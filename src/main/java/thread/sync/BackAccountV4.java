5178c7e3172b45ce47caf4cdbf195ed3aa9737d6
LooTan_H <loosung@naver.com>
2024-08-07 16:31:42 +0900

    Session 7 <고오급 동기화 : concurrent.Lock> 2. ReentrantLock : lock , unlock 적용을 할때는 무조건 반드시 Try-Finally 를 넣어줘야한다.

--- src/main/java/thread/sync/BackAccountV4.java
+++ src/main/java/thread/sync/BackAccountV4.java
@@ -1,10 +1,15 @@
 package thread.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static util.LoggerUtils.log;
 import static util.ThreadUtils.sleep;
 
 public class BackAccountV4 implements BackAccount {
 	private int balance;
+
+	private final Lock lock = new ReentrantLock();
 
 	public BackAccountV4(int initialBalance) {
 		this.balance = initialBalance;
@@ -14,18 +19,23 @@ public class BackAccountV4 implements BackAccount {
 	public boolean withdraw(int amount) {
 		log("거래 시작 : " + getClass().getSimpleName());
 
-		log("[검증 시작] 출금액 : " + amount + " 잔액 " + balance);
-		if (balance < amount) {
-			log("[검증 실패] 출금액 : " + amount + " 잔액 " + balance);
-			return false;
+		// ReentrantLock 이용
+		lock.lock();
+		try {
+			log("[검증 시작] 출금액 : " + amount + " 잔액 " + balance);
+			if (balance < amount) {
+				log("[검증 실패] 출금액 : " + amount + " 잔액 " + balance);
+				return false;
+			}
+			// 잔고가 출금액 보다 많으면, 진행
+			log("[검증 완료] 출금액 : " + amount + " 잔액 " + balance);
+
+			sleep(1000);
+			balance -= amount;
+			log("[출금 완료] 출금액 : " + amount + " 잔액 " + balance);
+		} finally {
+			lock.unlock();
 		}
-		// 잔고가 출금액 보다 많으면, 진행
-		log("[검증 완료] 출금액 : " + amount + " 잔액 " + balance);
-
-		sleep(1000);
-		balance -= amount;
-		log("[출금 완료] 출금액 : " + amount + " 잔액 " + balance);
-
 
 		log("거래 종료");
 		return true;
@@ -33,7 +43,12 @@ public class BackAccountV4 implements BackAccount {
 
 	@Override
 	public int getBalance() {
-		return balance;
+		lock.lock();
+		try {
+			return balance;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 }
