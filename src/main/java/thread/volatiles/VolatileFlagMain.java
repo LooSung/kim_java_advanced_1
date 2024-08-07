429ebad49eb70db48e3bc94bcc999aa14c7e9f30
LooTan_H <loosung@naver.com>
2024-08-07 09:44:49 +0900

    Session 5 <Volatile : 메모리 가시성> 3. volatile(메모리 가시성) : 메모리 가시성 문제를 위해 Volatile 를 넣어준다. 성능상 저하가 될 수도 있다고 한다.
    * volatile : "volatile" 할 당한 변수를 휘발성 메모리에 만들어라!

--- src/main/java/thread/volatiles/VolatileFlagMain.java
+++ src/main/java/thread/volatiles/VolatileFlagMain.java
@@ -22,14 +22,16 @@ public class VolatileFlagMain {
 
 	static class MyTask implements Runnable {
 		// "VolatileFlagMain" 메인 스레드는 절대로 멈추지 않았다... 언제 flag가 반영될지 모른다... 평생 while을 탈출을 못 할 수 도있다.
-		boolean flag = true;
+		volatile boolean flag = true;
 
 		@Override
 		public void run() {
 			log("task 시작");
 
 			while(flag) {
-
+				// 만약 while 문에 작업이 일어난다면 Context Switching 이 일어나면서, 메모리 가시성 문제가 해결 된다.
+				// 하지만 while이 언제 끝날지 모르니 꼭 volatile 을 써줘야한다.
+				System.out.println("Hello Thread");
 			}
 
 			log("task 종료");
