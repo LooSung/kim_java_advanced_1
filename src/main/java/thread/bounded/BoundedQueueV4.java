925ee78c81ffdcbc71154cd98cb2041e1828700f
LooTan_H <loosung@naver.com>
2024-08-09 09:58:28 +0900

    Session 9 생산자 소비자 문제 2 1. Lock Condition : 대기 스레드 집합을 서로 연결을 해주는 구현체
    condition vs wait
    : wait();는 자바 기본 Object 안에있는 대기 스레드에 넣지만, condition.await();는 Condition 객체에 넣는다.
    
    Condition은 ReentrantLock을 사용하는 스레드이다. Object.wait()와 유사한 기능이지만,
    자체 대기 집합을 만들어 지정한 대기중인 스레드를대기 그리고 꺠워주는 처리해준다.

---
+++ src/main/java/thread/bounded/BoundedQueueV4.java
@@ -0,0 +1,74 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.LoggerUtils.log;
+
+public class BoundedQueueV4 implements BoundedQueue {
+
+	private final Lock lock = new ReentrantLock();
+	private final Condition condition = lock.newCondition(); // 대기 집합.
+
+	private final Queue<String> queue = new ArrayDeque<>();
+	private final int max;
+
+	public BoundedQueueV4(int max) {
+		this.max = max;
+	}
+
+	@Override public void put(String data) {
+		lock.lock();
+
+		try {
+			while(queue.size() == max) {
+				log("[put] : Queue is full, so wait buffer");
+
+				try {
+					// wait(); 자바 Object 안에있는 대기 스레드에 넣는다.
+					condition.await();
+					log("[put] : producer wakeup");
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			queue.offer(data);
+			log("[put] : producer save data, notify()");
+			condition.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	@Override public String take() {
+		lock.lock();
+
+		try {
+			while(queue.isEmpty()) {
+				log("[take] Queue is empty, so wait buffer");
+				try {
+					condition.await();
+					log("[take] : consumer wakeup");
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			String data = queue.poll();
+			log("[put] : consumer get data, notify()");
+			condition.signal();
+
+			return data;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	@Override public String toString() {
+		return queue.toString();
+	}
+}
