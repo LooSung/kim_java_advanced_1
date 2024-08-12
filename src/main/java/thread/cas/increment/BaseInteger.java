a0730df354cd753c35a5017b51b2a3cec0a1f2a2
LooTan_H <loosung@naver.com>
2024-08-12 09:17:09 +0900

    Session 10 CAS - 동기화와 원자적 연산 1. 원자적 연산 - 성능 테스트
    Volatile을 해도 CPU 사이에 발생하는 캐시 메모리와 메인 메모리가 동기화가 되지 않아서, 1000이 나오지 않는다.
    
    Syncronized를 사용해서 안전한(한개씩 스레드가 돌게) 임계영역을 만들거나 Reentrente Lock을 만들어서 동시성문제를 해결하자

---
+++ src/main/java/thread/cas/increment/BaseInteger.java
@@ -0,0 +1,13 @@
+package thread.cas.increment;
+
+public class BaseInteger implements IncrementInteger {
+	private int value;
+
+	@Override public void increment() {
+		value++;
+	}
+
+	@Override public int get() {
+		return value;
+	}
+}
