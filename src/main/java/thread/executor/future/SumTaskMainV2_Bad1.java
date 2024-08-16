75f2a4f5c626daa10a4ce352b236a22bbcf9e718
LooTan_H <loosung@naver.com>
2024-08-16 12:38:07 +0900

    Session 12 ThreadPool & Executor 프레임워크 1 6. Future part.4 - 시용 이유 우리가 Future 없이 Callable에서 Return 되는 값을 사용한다면 어떻게 될 것인가?
    만약 future 없이 한다면 Main Thread에서 받은 작업 결과는 총 4초가 걸린다. 하지만 사용한다면 2초 정도가 줄여 줄 것이다. Bad_1,2를 보고 확인해보자.
    
    이유는 Thread-1, Thread-2가 Main Thread 에서 실행이 선 되고 future1.get()을 하면서 2초 대기 중일때, future2.get()은 이미 Main Thread 에서 실행 했기 때문에 다음 작업에서 즉시 반환이 될 것이다.

--- src/main/java/thread/executor/future/SumTaskMainV2_Bad1.java
+++ src/main/java/thread/executor/future/SumTaskMainV2_Bad1.java
@@ -5,23 +5,27 @@ import java.util.concurrent.*;
 import static util.LoggerUtils.log;
 import static util.ThreadUtils.sleep;
 
-public class SumTaskMainV2 {
+public class SumTaskMainV2_Bad1 {
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		SumTask task1 = new SumTask(1, 50);
 		SumTask task2 = new SumTask(51, 100);
 
 		ExecutorService es = Executors.newFixedThreadPool(2);
 
-		Future<Integer> future1 = es.submit(task1); // task 1 실행
-		Future<Integer> future2 = es.submit(task2); // task 2 실행
+		Future<Integer> future1 = es.submit(task1);
+		Integer sum1 = future1.get();
+		log("task1 = " + sum1);
+		// task1 작업 끝나고 2초 기다림
 
-		Integer sum1 = future1.get(); // future1.get() 실행 2초대기
-		Integer sum2 = future2.get(); // future2.get() 실행 2초 대기, 만약 완료 되었다면 즉시 반환
- 		log("task1 = " + sum1);
+		Future<Integer> future2 = es.submit(task2);
+		Integer sum2 = future2.get();
 		log("task2 = " + sum2);
+		// task2 작업 끝나고 2초 기다림
+
+		// 4초가 걸리면서 비효율이 된다.
 
 		int sumAll = sum1 + sum2;
-		log("task1 + task2 = " + sumAll); // 총 2초 걸림
+		log("task1 + task2 = " + sumAll);
 
 		es.close();
 		log("End");
